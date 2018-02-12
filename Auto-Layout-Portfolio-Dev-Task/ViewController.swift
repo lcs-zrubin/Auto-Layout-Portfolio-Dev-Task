f20465143436ff2c209d1e170fdc60685b0fe668
Russell Gordon <rgordon@lcs.on.ca>
2018-02-12 06:31:39 -0500

    Initial Commit

---
+++ Auto-Layout-Portfolio-Dev-Task/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto-Layout-Portfolio-Dev-Task
+//
+//  Created by Russell Gordon on 2/12/18.
+//  Copyright © 2018 Russell Gordon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
